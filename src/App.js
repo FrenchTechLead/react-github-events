7b0805bfaa9b708735eb2f83cc8f9a02a85e82ad
mecherim <mecheri.akram@gmail.com>
2017-05-19 12:01:17 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, {Component} from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap-theme.css";
-import "font-awesome/css/font-awesome.min.css";
-import FontAwesome from "react-fontawesome";
 import axios from "axios";
 import moment from "moment";
 import './App.css';
@@ -10,7 +6,7 @@ import './App.css';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {events: null, render: false, limit: 0};
+        this.state = {events: null, render: false,  limit:5};
 
     }
 
@@ -18,28 +14,28 @@ class App extends Component {
         axios.get("https://api.github.com/users/" + this.props.username + "/events")
             .then((events) => this.setState({events: events.data, render: true}))
             .catch((err) => console.log(err));
-        this.setState({limit:this.state.events.length})
+
     }
 
     lazyLoad() {
-        let x = this.state.limit - 5;
-        if (x <= 0)
-            x = 0;
-        this.setState({limit: x});
+        let limit = this.state.limit+5;
+        if(limit > this.state.events.length)
+            limit = this.state.events.length;
+        this.setState({limit:limit});
     }
 
     render() {
         let rows = [];
         if (this.state.render) {
-            for (let i = 0; i < this.state.events.length - this.state.limit; i++) {
+            for (let i = 0; i < this.state.limit ; i++) {
                 rows.push(
-                    <a key={i} href={"https://github.com/"+this.state.events[i].repo.name} target="_blank" className="list-group-item">
+                        <a key={i} href={"https://github.com/"+this.state.events[i].repo.name} target="_blank" className="list-group-item">
                         <h5 className="list-group-item-heading">
                             <div className="row">
                                 <div className="col-xs-2">
-                                    <img src={this.state.events[i].actor.avatar_url} width={30}/>
+                                    <img src={this.state.events[i].actor.avatar_url} className="avatar"/>
                                 </div>
-                                <div className="col-xs-8">
+                                <div className="col-xs-6 col-md-8">
                                     <span className="eventType">
                                         {this.state.events[i].type.substring(0, this.state.events[i].type.length - 5)}
                                     </span><br/>
@@ -48,7 +44,7 @@ class App extends Component {
                                     </span>
                                     <br/>
                                 </div>
-                                <div className="col-xs-2 timeBadgeContainer">
+                                <div className="col-xs-4 col-md-2 timeBadgeContainer">
                                     <span className="badge timeBadge">
                                         {moment( this.state.events[i].created_at).fromNow()}
                                     </span>
